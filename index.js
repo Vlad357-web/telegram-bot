2d2d7365f16f33b4c72f8f06082ab117c935e51d
Vlad <dovryankin66@gmail.com>
2022-12-16 20:04:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const { Telegraf, Markup } = require('telegraf')
+require('dotenv').config()
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.command('start', (ctx)=>{
+    ctx.reply('Hey! This is a template for you, study the functionality and give feedback.',{
+        reply_markup: Markup.keyboard([
+            '🔗│Social Networks',
+            '🤍│Whitepapper',
+            '❓️│FAQ',
+            '💲│Buy'
+        ])
+    })
+})
+
+
+bot.hears('🔗│Social Networks', ctx=>{
+    ctx.replyWithHTML(
+        'Telegram Chat - link here \n\n' + 
+        'Discord Server - <b>link here</b>'
+        )
+})
+bot.hears('🤍│Whitepapper',ctx=>{
+    ctx.replyWithHTML(
+        '<b>Whitepapper link here</b>'
+    )
+})
+bot.hears('❓️│FAQ', ctx=>{
+    ctx.replyWithHTML(
+        'Faq text here'
+    )
+})
+bot.hears('💲│Buy', ctx=>{
+    ctx.replyWithHTML(
+        'To buy a custom server, write here 👇 \n'+
+        '@vesternyachok'
+    )
+})
+
+
+bot.launch()
+
